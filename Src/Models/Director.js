828d2e3518589bb8567799f0f31423a685942aca
09X17 <72982944+09X17@users.noreply.github.com>
2025-09-24 18:41:17 -0500

    Update Director.js

--- Src/Models/Director.js
+++ Src/Models/Director.js
@@ -4,7 +4,9 @@ const directorSchema = new mongoose.Schema({
   nombres: { type: String, required: true },
   estado: { type: Boolean, default: true },
   fechaCreacion: { type: Date, default: Date.now },
-  fechaActualizacion: { type: Date, default: Date.now }
+  fechaActualizacion: { type: Date, default: Date.now },
+  edad: { type: Number },
+  nacionalidad: { type: String }
 });
 
 export default mongoose.model("Director", directorSchema);
